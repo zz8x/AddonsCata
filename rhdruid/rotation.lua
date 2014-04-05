0ef6716fa41807625a0beadc66851b7c8245d5f7
kluory <kluory@gmail.com>
2014-04-06 02:58:10 +0400

    test

--- rhdruid/rotation.lua
+++ rhdruid/rotation.lua
@@ -37,7 +37,7 @@ function Idle()
         -- if not HasDebuff("Устрашающий рев",3) and DoSpell("Устрашающий рев") then return end
     end
     if HasBuff("Облик кошки") then
-        if not HasBuff("Крадущийся зверь") and HasSpell("Звериный рывок(Облик медведя)") and IsAttack() and IsValidTarget("target") and InRange("Звериный рывок(Облик медведя)", "target") and GetSpellCooldownLeft("Звериный рывок(Облик кошки)") > 2 and GetSpellCooldownLeft("Звериный рывок(Облик медведя)") == 0 then
+        if not HasBuff("Крадущийся зверь") and HasSpell("Звериный рывок(Облик медведя)") and IsAttack() and IsValidTarget("target") and InRange("Звериный рывок(Облик медведя)", "target") and GetSpellCooldownLeft("Звериный рывок(облик кошки)") > 2 and GetSpellCooldownLeft("Звериный рывок(Облик медведя)") == 0 then
             UseMount("Облик медведя")
             return
         end
@@ -50,7 +50,7 @@ function Idle()
     
         if not (IsValidTarget("target") and (UnitAffectingCombat("target") or IsAttack()))  then return end
         
-        if IsAttack() and HasSpell("Звериный рывок(Облик кошки)") and (IsStealthed() or not IsReadySpell("Крадущийся зверь")) and DoSpell("Звериный рывок(Облик кошки)") then return end
+        if IsAttack() and HasSpell("Звериный рывок(облик кошки)") and (IsStealthed() or not IsReadySpell("Крадущийся зверь")) and DoSpell("Звериный рывок(облик кошки)") then return end
         
         if IsStealthed() then 
             
@@ -64,7 +64,7 @@ function Idle()
        
         
         
-        if InCombatLockdown() and IsAttack() and IsValidTarget("target") and InRange("Звериный рывок(Облик кошки)", "target") and DoSpell("Звериный рывок(Облик кошки)") then return end
+        if InCombatLockdown() and IsAttack() and IsValidTarget("target") and InRange("Звериный рывок(облик кошки)", "target") and DoSpell("Звериный рывок(облик кошки)") then return end
                 
         RunMacroText("/startattack")
 
@@ -79,7 +79,7 @@ function Idle()
         
         if UnitMana("player") < 30 and DoSpell("Тигриное неистовство") then return end
         
-        if HasDebuff("Глубокая рана") and HasDebuff("Волшебный огонь (облик зверя)", 5) and HasDebuff("Разорвать",7) and InMelee() then
+        if HasDebuff("Глубокая рана") and HasDebuff("Разорвать",7) and InMelee() then
             if UnitMana("player") > 25 and UnitMana("player") < 85 and HasSpell("Берсерк") and DoSpell("Берсерк") then return end
         end
         
