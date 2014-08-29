1c69395f2ec928a708a3074643d409555843bc24
zz8x <alvego+zz8x@gmail.com>
2014-08-30 02:56:57 +0400

    pal red dispel
    Conflicts:
    	rhpaladin/rotation.lua

--- rhpaladin/rotation.lua
+++ rhpaladin/rotation.lua
@@ -443,7 +443,7 @@ function HolyRotation()
 
     if h > 30 and IsReadySpell("Очищение") and UnitMana100("player") > 10 and IsSpellNotUsed("Очищение", 2) then
         for i = 1, #members do
-            if InControl(u) and TryDispel(members[i]) then return end
+            if InControl(members[i]) and TryDispel(members[i]) then return end
         end
     end
 
